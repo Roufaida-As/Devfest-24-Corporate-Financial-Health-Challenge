932c83278915ce0de081f82ca1c77e6f1fcbc7bd
Roufaida-As <r.asbar@esi-sba.dz>
2024-10-18 22:35:50 +0100

    dashboard/transactions/middlewares

--- server/models/insight_model.js
+++ server/models/insight_model.js
@@ -5,4 +5,5 @@ const InsightSchema = new mongoose.Schema({
     date: { type: Date, default: Date.now },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'USER' },
 });
+
 exports.module = mongoose.model('INSIGHT', InsightSchema)
